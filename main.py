d9e1ab9e1db6c7d21b743bfe0494e14032ec6215
Fedor Kruglikov <fedor.kruglickov@yandex.ru>
2026-03-20 15:41:54 +0300

    Добавить чтение AUTHOR из .env и вывод автора

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from dotenv import load_dotenv
+import os
+
 print('Hello from repository!')
+
+def print_author():
+    load_dotenv()
+    author = os.getenv("AUTHOR")
+    print(f"Автор проекта: {author}")
+
+if __name__ == "__main__":
+    print_author()
